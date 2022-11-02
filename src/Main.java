728641aae81f0978695fdd85dd67be7b3c619372
ducanhgtcc <ducanhgt.nd@gmail.com>
2022-11-02 23:34:28 +0700

    Tính chỉ số cân nặng của cơ thể

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your weight (in kilogram):");
+        double weight = scanner.nextDouble();
+
+        System.out.println("Your height (in meter):");
+        double height = scanner.nextDouble();
+
+        double bmi = weight / (height * height);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+
+        if (bmi < 18)
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+        else if (bmi < 25.0)
+            System.out.printf("%-20.2f%s", bmi, "Normal");
+        else if (bmi < 30.0)
+            System.out.printf("%-20.2f%s", bmi, "Overweight");
+        else
+            System.out.printf("%-20.2f%s", bmi, "Obese");
+    }
+}
